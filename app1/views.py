11fce48ab0acf5e64513a036d2aba0f4be985a8f
bharathpalagiri <bharathkumarreddy@gmail.com>
2022-12-25 20:17:10 +0530

    first commit

---
+++ app1/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+
+# Create your views here.
+def first(request):
+    return render(request,'first.html')
+
+
+def bharath(request):
+    return render(request,'bharath.html')
